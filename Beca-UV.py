9e8c68cc5d5ad97e60fb01c7d6dd49f729a3c961
Moretonx <franciscomoretti327@gmail.com>
2025-12-15 03:19:14 -0300

    fix interfaz

--- Beca-UV.py
+++ Beca-UV.py
@@ -298,7 +298,7 @@ titulo0 = tkinter.Label(
 )
 titulo0.pack()
 
-contexto["imagen_desea"] = PhotoImage(file="img/desea.png").subsample(2, 2)
+#contexto["imagen_desea"] = PhotoImage(file="img/desea.png").subsample(2, 2)
 tkinter.Label(contexto["ventana"], image=contexto["imagen_desea"], bg="white").pack()
 
 # Cargar casinos
